83019c40de197dc2c28e6a82e5ca9c3665fbcdfe
vivian <vivian.xmt@gmail.com>
2015-01-15 10:16:39 +0800

    AtomicInteger

---
+++ AtomicVariable.java
@@ -0,0 +1,54 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+// basic class
+class Counter {
+    private int c = 0;
+
+    public void increment() {
+        c++;
+    }
+
+    public void decrement() {
+        c--;
+    }
+
+    public int value() {
+        return c;
+    }
+
+}
+
+// use the synchronized methods
+class SynchronizedCounter {
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+
+    public synchronized void decrement() {
+        c--;
+    }
+
+    public synchronized int value() {
+        return c;
+    }
+
+}
+
+// use the AtomicInteger
+public class AtomicVariable {
+    private AtomicInteger c = new AtomicInteger(0);
+
+    public void increment() {
+        c.incrementAndGet();
+    }
+
+    public void decrement() {
+        c.decrementAndGet();
+    }
+
+    public int value() {
+        return c.get();
+    }
+}
